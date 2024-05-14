32811e079cf3e0d8c6dfc252d90a6b79a462e4bd
Flavio Coscarella <flaviodc@live.com.ar>
2024-05-14 11:04:30 -0300

    adding middleware for JWT

---
+++ src/infra/middleware/logger.middleware.ts
@@ -0,0 +1,26 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      res.sendStatus(401);
+      return;
+    }
+
+    try {
+      const decodedToken = jwt.decode(token) as any;
+      if (decodedToken.general_email) {
+        req.body.userEmail = decodedToken.general_email;
+        next();
+      } else {
+        res.sendStatus(403);
+      }
+    } catch {
+      res.sendStatus(403);
+    }
+  }
+}
