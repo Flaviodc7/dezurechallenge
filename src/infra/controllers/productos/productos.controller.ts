add180b8063b3d5c6e02b1af4d3f66ffe995cfbb
Flavio Coscarella <flaviodc@live.com.ar>
2024-05-13 22:31:52 -0300

    Creating controllers for productos and usuarios

--- src/infra/controllers/productos/productos.controller.ts
+++ src/infra/controllers/productos/productos.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Put,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { CreateProductoDTO, UpdateProductoDTO } from '../../dtos/productos.dto';
+import { ProductosService } from '../../services/productos/productos.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Productos')
 @Controller('productos')
-export class ProductosController {}
+export class ProductosController {
+  constructor(private productosService: ProductosService) {}
+
+  @ApiOperation({ summary: 'Obtener un producto por ID' })
+  @Get(':idProducto')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getProducto(@Param('idProducto') idProducto: string) {
+    return this.productosService.findOne(idProducto);
+  }
+
+  @ApiOperation({ summary: 'Crear un producto' })
+  @Post()
+  create(@Body() payload: CreateProductoDTO) {
+    return this.productosService.create(payload);
+  }
+
+  @ApiOperation({ summary: 'Modificar un producto' })
+  @Put()
+  update(@Body() payload: UpdateProductoDTO) {
+    return this.productosService.update(payload);
+  }
+
+  @ApiOperation({ summary: 'Eliminar un producto' })
+  @Delete('/:idProducto')
+  deleteProduct(@Param('idProducto') idProducto: string): any {
+    return {
+      idProducto,
+      delete: true,
+      count: 1,
+    };
+  }
+}
--- src/infra/controllers/usuarios/usuarios.controller.ts
+++ src/infra/controllers/usuarios/usuarios.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Put,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { CreateUsuarioDTO, UpdateUsuarioDTO } from '../../dtos/usuarios.dto';
+import { UsuariosService } from '../../services/usuarios/usuarios.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Usuarios')
 @Controller('usuarios')
-export class UsuariosController {}
+export class UsuariosController {
+  constructor(private usuariosService: UsuariosService) {}
+
+  @ApiOperation({ summary: 'Obtener un usuario por ID' })
+  @Get(':idUsuario')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getUsuario(@Param('idUsuario') idUsuario: string) {
+    return this.usuariosService.findOne(idUsuario);
+  }
+
+  @ApiOperation({ summary: 'Crear un usuario' })
+  @Post()
+  create(@Body() payload: CreateUsuarioDTO) {
+    return this.usuariosService.create(payload);
+  }
+
+  @ApiOperation({ summary: 'Modificar un usuario' })
+  @Put()
+  update(@Body() payload: UpdateUsuarioDTO) {
+    return this.usuariosService.update(payload);
+  }
+
+  @ApiOperation({ summary: 'Eliminar un usuario' })
+  @Delete('/:idUsuario')
+  deleteUsuario(@Param('idUsuario') idUsuario: string): any {
+    return {
+      idUsuario,
+      delete: true,
+      count: 1,
+    };
+  }
+}
